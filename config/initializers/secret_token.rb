2a0906d6fefd8e7e6c7dae4cc7af16858fba8289
Pedro Sanchez <pdsanch1@gmail.com>
2012-12-01 15:51:07 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DansWebsite::Application.config.secret_token = '500daf61a499b8e1f86e1064f4c19894025157cd6e656de77dcc720ce7362994e009fc1c3a2be6a005ce3d86674fabc67bb3703c528731ffa81f0dceb51f4bd4'
