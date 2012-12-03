a42794166426d34f6d616649c286f6996138228f
Pedro Sanchez <pdsanch1@gmail.com>
2012-12-02 21:37:16 -0500

    now using Sass stylesheets

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module DansWebsite
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.paths << Rails.root.join("fonts")
   end
 end
